ee5a4bd7b8502e9b342b91cba2cadaa7d509f121
CassianoCardoso <dj_kassiano@hotmail.com>
2022-04-13 10:30:56 -0300

    Intro PHP

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+
+  <p>FORMULARIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+
+  <form action="script.php" method="POST">
+    <p> Entre com o Nome:<input type="text" name="nome" /></p>
+    <p>Entre com a Idade: <input type="text" name="idade" /></p>
+    <p> <input type="submit"value="Enviar dados do competidor" /> </p>
+
+  </form>
+</body>
+
+</html>
